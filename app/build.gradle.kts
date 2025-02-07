59e19c6f0f462e869d62cb57c3bf2b115433de6e
2sseul <iseul0626@gmail.com>
2025-02-08 02:38:42 +0900

    Feat: v1.0 배포 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.appcompat)
+    implementation(libs.firebase.messaging.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
