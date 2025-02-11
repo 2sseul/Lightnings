b22758d4fa5e2834db9a02ac2bc996077b78f93d
2sseul <iseul0626@gmail.com>
2025-02-12 02:59:03 +0900

    Fix: 하단 네비게이션 이동 에러

--- app/src/main/java/com/my_app/lightning/SettingsAcivity.kt
+++ app/src/main/java/com/my_app/lightning/SettingsAcivity.kt
@@ -1,6 +1,9 @@
 package com.my_app.lightning
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsAcivity : AppCompatActivity() {
@@ -9,5 +12,14 @@ class SettingsAcivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting)
 
+        findViewById<ImageView>(R.id.bookmark).setOnClickListener {
+            startActivity(Intent(this, BookmarkActivity::class.java))
+        }
+        findViewById<ImageView>(R.id.btnAdd).setOnClickListener {
+            startActivity(Intent(this, AddList::class.java))
+        }
+        findViewById<ImageView>(R.id.settings).setOnClickListener {
+            startActivity(Intent(this, SettingsAcivity::class.java))
+        }
     }
 }
