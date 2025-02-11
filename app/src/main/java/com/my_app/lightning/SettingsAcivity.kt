29bdb4ca4f3f0ca8a12992f3d46f791790ffb310
2sseul <iseul0626@gmail.com>
2025-02-11 14:18:49 +0900

    Fix: 설정 페이지 안 들어가지는 이슈 해결

---
+++ app/src/main/java/com/my_app/lightning/SettingsAcivity.kt
@@ -0,0 +1,13 @@
+package com.my_app.lightning
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsAcivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.setting)
+
+    }
+}
