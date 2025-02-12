b717243036643fae5b47586e40d9756ce1c7ad2a
2sseul <iseul0626@gmail.com>
2025-02-12 22:07:42 +0900

    Feat: 설명서 링크 이동 추가

--- app/src/main/java/com/my_app/lightning/SettingsAcivity.kt
+++ app/src/main/java/com/my_app/lightning/SettingsAcivity.kt
@@ -1,8 +1,10 @@
 package com.my_app.lightning
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
@@ -20,6 +22,13 @@ class SettingsAcivity : AppCompatActivity() {
         }
         findViewById<ImageView>(R.id.settings).setOnClickListener {
             startActivity(Intent(this, SettingsAcivity::class.java))
+        }
+
+        // 설명서 클릭 시 Notion 페이지 열기
+        findViewById<LinearLayout>(R.id.manualSection).setOnClickListener {
+            val url = "https://principled-saturn-9fe.notion.site/8c0bbb5f50b04a849d0405e122b14648"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
         }
     }
 }
