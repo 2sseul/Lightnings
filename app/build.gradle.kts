60a0dfafbdf8189e157ce269040492e0481b5e5d
2sseul <iseul0626@gmail.com>
2025-02-17 00:55:31 +0900

    Fix: version fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.my_app.lightning"
         minSdk = 25
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
